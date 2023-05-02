2806464aeee041352f556015e906f08dfcd8eefc
developer-nayem <mdalimodassernayem@gmail.com>
2023-05-02 08:25:57 +0600

    create data

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const app = express();
 
 // data
+const chefs = require("./data/chefs.json");
+const recipes = require("./data/recipes.json");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +12,20 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("<h1>Chef's Magic Server is Running...</h1>");
+});
+
+app.get("/chefs", (req, res) => {
+   res.json(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+   const { id } = req.params;
+   const chef = chefs.find((chef) => chef._id === id);
+   const chefRecipes = recipes.filter((recipe) => recipe.author === id);
+   res.json({
+      chef,
+      chefRecipes,
+   });
 });
 
 const port = process.env.PORT || 5000;
